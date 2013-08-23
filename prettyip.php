755111fb7e4e7bfa409bd8c2aa5bd86020556c62
graypants <liuorhui@gmail.com>
2013-08-23 18:42:46 +0800

    README.md

--- prettyip.php
+++ prettyip.php
@@ -1,16 +1,39 @@
 <?php 
 
+/**
+ *
+ * PrettyIP 
+ *
+ * Author: graypants
+ * Email: liuorhui@gmail.com
+ * 
+ **/
 
 class PrettyIP {
 
-public function PrettyIP() { 
+	public static function clientIP() {
+
+	    if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown")) {
+	        $ip = getenv("HTTP_CLIENT_IP");
+	    }
+	    elseif (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown")) {
+	        $ip = getenv("HTTP_X_FORWARDED_FOR");
+	    }
+	    elseif (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown")) {
+	        $ip = getenv("REMOTE_ADDR");
+	    }
+	    elseif (isset($_SERVER["REMOTE_ADDR"]) && $_SERVER["REMOTE_ADDR"] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown")) {
+	        $ip = $_SERVER["REMOTE_ADDR"];
+	    }
+	    else {
+	        $ip = "0.0.0.0";
+	    }
+	    return $ip;
+	}
 
 }
 
-public function mobileIP() {
+phpinfo();
 
-}
-
-}
-
+	
 ?>
